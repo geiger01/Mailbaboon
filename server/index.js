f0d9244d677085752027cf3fe94e5167e9e56971
geiger01 <tantangeiger@gmail.com>
2022-01-14 22:58:59 +0200

    test

--- server/index.js
+++ server/index.js
@@ -5,5 +5,7 @@ const app = express();
 
 require('./routes/authRoutes')(app)
 
+
 const PORT = process.env.PORT || 5001;
 app.listen(PORT);
+                                                                            
