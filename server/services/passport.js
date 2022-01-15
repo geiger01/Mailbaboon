440171de978bf29daee2584c3fedd26b3474cabf
geiger01 <tantangeiger@gmail.com>
2022-01-15 17:22:58 +0200

    connection to atlas and add user from google done

--- server/services/passport.js
+++ server/services/passport.js
@@ -1,17 +1,19 @@
 const passport = require("passport");
 const GoogleStrategy = require("passport-google-oauth20").Strategy;
 const keys = require("../config/keys");
+const mongoose = require("mongoose");
+
+const User = mongoose.model("users");
 
 passport.use(
-    new GoogleStrategy(
-      {
-        clientID: keys.googleClientID,
-        clientSecret: keys.googleClientSecret,
-        callbackURL: "/auth/google/callback",
-      },
-      (accessToken) => {
-          console.log(accessToken);
-      }
-    )
-  );
-  
+  new GoogleStrategy(
+    {
+      clientID: keys.googleClientID,
+      clientSecret: keys.googleClientSecret,
+      callbackURL: "/auth/google/callback",
+    },
+    (request, accessToken, refreshToken, proflie, done) => {
+      new User({ googleId: proflie.id }).save();
+    }
+  )
+);
