8460b1510566ff7d9702a5a73d3cdedd36678ffc
geiger01 <tantangeiger@gmail.com>
2022-01-29 21:29:35 +0200

    \added auth middlware

--- server/routes/stripeRoutes.js
+++ server/routes/stripeRoutes.js
@@ -1,11 +1,13 @@
 const keys = require("../config/keys");
 const stripe = require("stripe")(keys.stripeSecretKey);
+const requireLogin = require("../middlewares/requireLogin");
 
 module.exports = (app) => {
-  app.post("/api/stripe", async (req, res) => {
+  app.post("/api/stripe", requireLogin, async (req, res) => {
+
     const charge = await stripe.charges.create({
       amount: 500,
-      currency: "use",
+      currency: "usd",
       description: "Just give me money, why not?",
       source: req.body.id,
     });
